adb328268c870d7b2a06f528239fe68794d3c37c
Hekimianz <aramhekdesign@gmail.com>
2023-09-17 19:19:25 -0600

    Implemented Add Book Feature

--- src/app.js
+++ src/app.js
@@ -1,17 +1,7 @@
-const library = [
-  {
-    title: "The Hobbit",
-    author: "J.R.R. Tolkien",
-    numPages: 495,
-    readStatus: true,
-  },
-  {
-    title: "Project Hail Mary",
-    author: "Andy Weir",
-    numPages: 695,
-    readStatus: true,
-  },
-];
+const newBookBtn = document.querySelector(".addBook");
+const shelf = document.querySelector(".shelf");
+
+const library = [];
 
 const Book = function (title, author, numPages, readStatus) {
   this.title = title;
@@ -20,13 +10,67 @@ const Book = function (title, author, numPages, readStatus) {
   this.readStatus = readStatus;
 };
 
-const addBookToLibrary = function () {
-  const title = prompt("Title:");
-  const author = prompt("Author:");
-  const numPages = prompt("Number of Pages:");
-  const readStatus = prompt("Read Status:");
-
-  library.push(new Book(title, author, numPages, readStatus));
+const renderLibrary = function () {
+  shelf.innerHTML = "";
+  library.forEach((book) => {
+    const markup = `<div class="book--wrapper ">
+    <span class="book--status ${book.readStatus}"></span>
+    <p class="book--title book--data">${book.title}</p>
+    <p class="book--author book--data">${book.author}</p>
+    <p class="book--pages book--data">${book.numPages} pages</p>
+    <span class="material-icons book--edit">edit</span>
+    <span class="material-icons book--delete">delete</span>
+  </div>`;
+    shelf.insertAdjacentHTML("beforeend", markup);
+  });
 };
 
-// addBookToLibrary();
+const showNewBookForm = function () {
+  const books = document.querySelectorAll(".book--wrapper");
+  if (document.querySelector(".new")) return;
+  const markup = `<div class="book--wrapper new">
+  <span class="book--status reading"></span>
+  <input
+    type="text"
+    placeholder="Title"
+    class="book--title book--data"
+  />
+  <input
+    type="text"
+    placeholder="Author"
+    class="book--author book--data"
+  />
+  <input type="number" placeholder="0" class="book--pages book--data" />
+  <span class="material-icons confirmBook">add</span>
+  <span class="material-icons book--delete">delete</span>
+</div>`;
+
+  shelf.insertAdjacentHTML("afterend", markup);
+  const newBook = document.querySelector(".new");
+  const confirmBook = newBook.querySelector(".confirmBook");
+  confirmBook.addEventListener("click", addBookToLibrary);
+};
+
+const addBookToLibrary = function () {
+  const newBook = document.querySelector(".new");
+  const newBookTitle = newBook.querySelector(".book--title").value;
+  const newBookAuthor = newBook.querySelector(".book--author").value;
+  const newBookPages = newBook.querySelector(".book--pages").value;
+  let newBookStatus = newBook.querySelector(".book--status");
+  if (newBookStatus.classList.contains("read")) {
+    newBookStatus = "read";
+  } else if (newBookStatus.classList.contains("notRead")) {
+    newBookStatus = "notRead";
+  } else {
+    newBookStatus = "reading";
+  }
+
+  library.push(
+    new Book(newBookTitle, newBookAuthor, newBookPages, newBookStatus)
+  );
+  console.log(library);
+  renderLibrary();
+  newBook.remove();
+};
+
+newBookBtn.addEventListener("click", showNewBookForm);
